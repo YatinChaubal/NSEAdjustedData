48753883351161f8d96104c28cf333e66e48a0b6
root <root@budgetvm.ping2ping.in>
2014-12-23 07:50:21 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from nsedownload import NseBhavcopy
+import datetime
+import csv
+
+#OPEN,HIGH,LOW,CLOSE,VOLUME
+
+
+if __name__ == "__main__":
+    i=NseBhavcopy()
+    for j in range(1,23):
+        dt=datetime.date(2014,12,j)
+        print "downloading S"+str(dt)
+        i.GetBhavcopy(datetime.date(2014,12,j))
+        try:
+            fname = open("bhavcopcm"+dt.strftime("%d%b%Y")+"bhav.csv")
+        except:
+            continue
+
+        today_stocks_csv = csv.reader(fname)
+        for each_stock in today_stocks_csv:
+
+            if each_stock[0] == "":
+                continue
+            print each_stock[0]
+
+            stock_csv_file = open(str(each_stock[0]+".csv"),"a+")
+            stock_csv_file.write(each_stock[1]+","+each_stock[2]+","+each_stock[3]+","+each_stock[4]+
+                                ","+each_stock[5]+","+each_stock[6]+"\n")
+
+
+
+
+
---
+++ nsedownload.py
@@ -0,0 +1,94 @@
+import urllib2
+import datetime
+import zipfile
+import os
+import csv
+#from win32com.client import Dispatch
+
+
+
+class NseBhavcopy:
+	def __init__(self):
+
+		self.hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+			'Accept-Encoding': 'none',
+			'Accept-Language': 'en-US,en;q=0.8',
+			'Connection': 'keep-alive'}
+
+	def converttoBhav(self,name,curdt):
+		ifile  = open(name, "rb")
+		reader = csv.reader(ifile)
+
+		f=open("bhavcop"+name,"w")
+		next(reader, None)
+		for row in reader:
+
+
+			f.write(row[0] + "," +
+			curdt.strftime("%Y%m%d") +
+			"," + row[2] + "," + row[3] +
+			"," + row[4] + "," + row[5] +
+			"," + row[8] + "\n")
+		ifile.close()
+
+	def GetBhavcopy(self,Date=datetime.date.today()):
+		curdt=Date
+		yy=curdt.year
+		mm=curdt.strftime("%b")
+		dd=curdt.day
+
+		if dd < 10:
+			dd ="0"+str(dd)
+		url="http://www.nseindia.com/content/historical/EQUITIES/"+str(yy)+"/"+str(mm).upper()+"/cm"+str(dd)+str(mm).upper()+str(yy)+"bhav.csv.zip"
+
+		req = urllib2.Request(url, headers=self.hdr)
+
+		fileavailable=1
+		try:
+			page = urllib2.urlopen(req)
+		except urllib2.HTTPError, e:
+			#print e.fp.read()
+			fileavailable=0
+			self.filename=""
+			self.ValidData=0
+
+
+		if fileavailable == 1:
+			meta = page.info()
+			file_size = int(meta.getheaders("Content-Length")[0])
+			file_name = url.split('/')[-1]
+			f = open(file_name, 'wb')
+			file_size_dl = 0
+			block_sz = 8192
+			while True:
+				buffer = page.read(block_sz)
+				if not buffer:
+					break
+
+				file_size_dl += len(buffer)
+				f.write(buffer)
+				status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+				status = status + chr(8)*(len(status)+1)
+			f.close()
+
+			zfile=open(file_name, 'rb')
+			z=zipfile.ZipFile(zfile)
+			for name in z.namelist():
+				outfile = open(name, 'wb')
+				outfile.write(z.read(name))
+				outfile.close()
+				self.converttoBhav(name,curdt)
+			zfile.close()
+			self.filename=name
+			self.ValidData=1
+			os.remove(name)
+			os.remove(file_name)
+
+
+
+
+
+
+
