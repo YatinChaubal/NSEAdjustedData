298d7eb9bcc0b416de5940ed42e3acec9fe84df9
YatinChaubal <yatin.chaubal@gmail.com>
2014-12-24 18:27:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from nsedownload import NseBhavcopy
 import datetime
 import csv
+import thread
 
 #OPEN,HIGH,LOW,CLOSE,VOLUME
 
 
 if __name__ == "__main__":
     i=NseBhavcopy()
-    for j in range(1,23):
+    for j in range(1,30):
         dt=datetime.date(2014,12,j)
         print "downloading S"+str(dt)
-        i.GetBhavcopy(datetime.date(2014,12,j))
+        i.GetBhavcopy(datetime.date(2014,11,j))
+        '''
         try:
             fname = open("bhavcopcm"+dt.strftime("%d%b%Y")+"bhav.csv")
         except:
@@ -26,8 +28,7 @@ if __name__ == "__main__":
             stock_csv_file = open(str(each_stock[0]+".csv"),"a+")
             stock_csv_file.write(each_stock[1]+","+each_stock[2]+","+each_stock[3]+","+each_stock[4]+
                                 ","+each_stock[5]+","+each_stock[6]+"\n")
-
-
+'''
 
 
 
