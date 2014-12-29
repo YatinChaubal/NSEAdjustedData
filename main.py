cbdc463b76d0c35be23994464897bbfe46302ba4
YatinChaubal <yatin.chaubal@gmail.com>
2014-12-29 20:36:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-from nsedownload import NseBhavcopy
+from NSESQL import NSESQL
 import datetime
+import time
 import csv
 import thread
+from nsedownload import NseDownload
+import threading
+import sys
 
 #OPEN,HIGH,LOW,CLOSE,VOLUME
 
 
+def DBBulkUpdate(fname):
+    i=NSESQL()
+    i.CSVToDB(fname)
+
+
+
+
 if __name__ == "__main__":
-    i=NseBhavcopy()
-    for j in range(1,30):
-        dt=datetime.date(2014,12,j)
-        print "downloading S"+str(dt)
-        i.GetBhavcopy(datetime.date(2014,11,j))
-        '''
-        try:
-            fname = open("bhavcopcm"+dt.strftime("%d%b%Y")+"bhav.csv")
-        except:
-            continue
+    i=NSESQL()
+#    i.CSVToDB("cm07JUL2004bhav.csv")
 
-        today_stocks_csv = csv.reader(fname)
-        for each_stock in today_stocks_csv:
+    #i=NseDownload()
 
-            if each_stock[0] == "":
-                continue
-            print each_stock[0]
+    yy=int(sys.argv[1])
+    #yy=2003
+    print yy
+    last_update=time.strptime(i.GetLastUpdate(yy),"%Y-%m-%d")
+    dt=datetime.date(last_update.tm_year,last_update.tm_mon,last_update.tm_mday)
+    print "Starting at "+str(dt)
 
-            stock_csv_file = open(str(each_stock[0]+".csv"),"a+")
-            stock_csv_file.write(each_stock[1]+","+each_stock[2]+","+each_stock[3]+","+each_stock[4]+
-                                ","+each_stock[5]+","+each_stock[6]+"\n")
-'''
+    thread_cnt=0
+    while dt <= datetime.date(yy,12,31):
 
+        fname="cm"+dt.strftime("%d%b%Y").upper()+"bhav.csv"
 
+        print "Processing "+fname
+        print datetime.datetime.now().time()
+
+        i.CSVToDB(fname)
+        dt=dt+ datetime.timedelta(days=1)
 
